aafecb3a7bb09cc5cb5af7ce2517317609f145d4
ryanG-543 <ryangiang29@gmail.com>
2017-04-12 12:10:46 -0400

    Added a background and Sprite.cs

--- Keep_Up/Game1.cs
+++ Keep_Up/Game1.cs
@@ -32,7 +32,7 @@ namespace Keep_Up
         /// This is where it can query for any required services and load any non-graphic
         /// related content.  Calling base.Initialize will enumerate through any components
         /// and initialize them as well.
-        /// </summary>
+        ///  </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
